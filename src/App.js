8c118ca2245ab6cda3aaeef82ad930e29a265723
Bryan <windsor.bryan@gmail.com>
2019-01-04 17:22:55 +0100

    setup basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Clarifai from "clarifai";
+import Navigation from "./Navigation/Navigation";
+import Logo from "./Logo/Logo";
+import Particles from "react-particles-js";
+import Rank from "./Rank/Rank";
+import ImageLinkForm from "./ImageLinkForm/ImageLinkForm";
+import "tachyons";
+import "./App.css";
+
+const app = new Clarifai.App({
+  apiKey: "d3a7e49e40bd4319a07e8fe9877e0b17"
+});
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = event => {
+    console.log(event.type);
+    app.models
+      .predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        "https://samples.clarifai.com/face-det.jpg"
+      )
+      .then(
+        function(response) {
+          // do something with response
+        },
+        function(err) {
+          // there was an error
+        }
+      );
+  };
+
   render() {
+    const particlesOptions = {
+      particles: {
+        number: {
+          value: 189,
+          density: {
+            enable: true,
+            value_area: 1262.6362266116362
+          }
+        },
+        color: {
+          value: "#000000"
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#000000"
+          },
+          polygon: {
+            nb_sides: 5
+          },
+          image: {
+            src: "img/github.svg",
+            width: 100,
+            height: 100
+          }
+        },
+        opacity: {
+          value: 0.8522794529628545,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
+          }
+        },
+        size: {
+          value: 2,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 40,
+            size_min: 0.1,
+            sync: false
+          }
+        },
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: "#c8c0c0",
+          opacity: 0.4,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 6,
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
+          }
+        }
+      }
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/*<FaceRecognition />*/}
       </div>
     );
   }
---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "tachyons";
+import "./Navigation.css";
+
+const Navigation = () => {
+  return (
+    <nav className="">
+      <p className="f3 link dim tr gray underline pa3 pointer ml0">Sign Out</p>
+    </nav>
+  );
+};
+
+export default Navigation;
